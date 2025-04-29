f5bd8423af045681c36b87f71b54e9e1dffc55cf
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-04-29 11:32:31 +0200

    structs + actual sync logic + error codes (well for now just one)

--- src/core/libraries/fios2/fios2.h
+++ src/core/libraries/fios2/fios2.h
@@ -11,5 +11,62 @@ class SymbolsResolver;
 
 namespace Libraries::Fios2 {
 
+typedef s32 OrbisFiosHandle;
+typedef OrbisFiosHandle OrbisFiosFH; // file handle
+typedef OrbisFiosHandle OrbisFiosDH; // directory handle
+typedef OrbisFiosHandle OrbisFiosOp;
+
+typedef u8 OrbisFiosOpEvent;
+
+typedef s64 OrbisFiosOffset;
+typedef s64 OrbisFiosDate;
+typedef s64 OrbisFiosTime;
+
+typedef struct OrbisFiosStat {
+    OrbisFiosOffset fileSize;
+    OrbisFiosDate accessDate;
+    OrbisFiosDate modificationDate;
+    OrbisFiosDate creationDate;
+    u32 statFlags;
+    u32 reserved;
+    s64 uid;
+    s64 gid;
+    s64 dev;
+    s64 ino;
+    s64 mode;
+} OrbisFiosStat;
+
+typedef struct OrbisFiosBuffer {
+    void* pPtr;
+    u64 length;
+} OrbisFiosBuffer;
+
+typedef struct OrbisFiosOpenParams {
+    u32 openFlags : 16;
+    u32 opFlags : 16;
+    u32 reserved;
+    OrbisFiosBuffer buffer;
+} OrbisFiosOpenParams;
+
+typedef int (*OrbisFiosOpCallback)(
+    void *pContext,
+    OrbisFiosOp op,
+    OrbisFiosOpEvent event,
+    int err
+);
+
+typedef struct OrbisFiosOpAttr {
+    OrbisFiosTime deadline;
+    OrbisFiosOpCallback pCallback;
+    void *pCallbackContext;
+    int32_t priority:8;
+    uint32_t opflags:24;
+    uint32_t userTag;
+    void *userPtr;
+    void *pReserved;
+} OrbisFiosOpAttr;
+
+s32 PS4_SYSV_ABI sceFiosOpSyncWait(OrbisFiosOp op);
+
 void RegisterlibSceFios2(Core::Loader::SymbolsResolver* sym);
 } // namespace Libraries::Fios2
---
+++ src/core/libraries/fios2/fios2_error.h
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "core/libraries/error_codes.h"
+
+// Fios library
+constexpr int SCE_FIOS_ERROR_BAD_OP = 0x8082000A;
