5ed05c67dae6da76cbb36eae065be98e1034b69f
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-06-28 11:14:03 +0200

    Add example hooking to Driveclub

--- src/core/game_hooks/dc_hooks.cpp
+++ src/core/game_hooks/dc_hooks.cpp
@@ -15,13 +15,14 @@ namespace DCHooks {
 
 static bool initted = false;
 static u64 EBOOT_MODULE_BASE;
+constexpr u64 GHIDRA_OFFSET_ADJUSTMENT = 0x00400000;
 
-constexpr auto DC_fun = 0x12345;
-static HookInformation _fun_hook = {};
+constexpr auto DC_DebugLog = 0x0126f1f0;
+static HookInformation _DebugLog_hook = {};
 
-void SHAD_NO_INLINE PS4_SYSV_ABI HOOK_fun(void* a1) {
+void SHAD_NO_INLINE PS4_SYSV_ABI HOOK_DebugLog(void* a1) {
 
-    LOG_INFO(Core_Linker, "{}", "a");
+    LOG_INFO(Core_Linker, "{}", "test for hooking");
 }
 
 void Initialize(Core::Module* mainModule) {
@@ -31,7 +32,7 @@ void Initialize(Core::Module* mainModule) {
 
     EBOOT_MODULE_BASE = mainModule->GetBaseAddress();
 
-    _fun_hook = CreateHook(reinterpret_cast<void*>(EBOOT_MODULE_BASE + DC_fun), (void*)&HOOK_fun); EnableHook(&_fun_hook);
+    _DebugLog_hook = CreateHook(reinterpret_cast<void*>(EBOOT_MODULE_BASE - GHIDRA_OFFSET_ADJUSTMENT + DC_DebugLog), (void*)&HOOK_DebugLog); EnableHook(&_DebugLog_hook);
 
     initted = true;
 }
