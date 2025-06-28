2dbe4a8631859b5bb3a0fa70025d6a157655b7de
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-06-28 13:01:43 +0200

    More stuff

--- src/core/game_hooks/dc_hooks.cpp
+++ src/core/game_hooks/dc_hooks.cpp
@@ -27,10 +27,9 @@ void SHAD_NO_INLINE PS4_SYSV_ABI HOOK_DebugLog(void* a1) {
     LOG_INFO(Core_Hooking, "test for hooking");
 }
 
-u64 SHAD_NO_INLINE PS4_SYSV_ABI HOOK_SearchAndDoSomethingToMemoryByName(u8* searched_array_struct,
-                                                                        char* name) {
-    LOG_INFO(Core_Hooking, "struct ptr: {}, searched name: {}", fmt::ptr(searched_array_struct),
-             name);
+u64 SHAD_NO_INLINE PS4_SYSV_ABI HOOK_QueryHeapSize(u8* searched_array_struct, char* name) {
+    LOG_INFO(Core_Hooking, "struct ptr: {}, searched name: {}, flags: {}",
+             fmt::ptr(searched_array_struct), name, *(searched_array_struct + 4));
     u64* searched_array = *(u64**)(searched_array_struct + 0x820);
     u32 array_size = *(u32*)(searched_array_struct + 0x818);
 
@@ -39,22 +38,46 @@ u64 SHAD_NO_INLINE PS4_SYSV_ABI HOOK_SearchAndDoSomethingToMemoryByName(u8* sear
     // for (int i = 0; i < array_size; i++) {
     //     LOG_INFO(Core_Hooking, " Heap name {}: {}", i, (char*)searched_array[i * 9]);
     // }
-    /* binary search? */
-    // u64 uVar4;
-    // while (uVar4 = index, 0 < (long)uVar4) {
-    //     index = (long)uVar4 / 2;
-    //     LOG_INFO(Core_Hooking, "Comparing \"{}\" to {}", (char*)element_ptr[index * 9], name);
-    //     s32 iVar2 = strcmp((char*)element_ptr[index * 9], name);
-    //     if (iVar2 < 0) {
-    //         element_ptr = element_ptr + index * 9 + 9;
-    //         index = (uVar4 - 1) - index;
-    //     }
-    // }
+
+    u64 binary_search_local;
+    while (binary_search_local = index, 0 < (long)binary_search_local) {
+        index = (long)binary_search_local / 2;
+        // LOG_INFO(Core_Hooking, "Comparing \"{}\" to {}", (char*)element_ptr[index * 9], name);
+        s32 iVar2 = strcmp((char*)element_ptr[index * 9], name);
+        if (iVar2 < 0) {
+            element_ptr = element_ptr + index * 9 + 9;
+            index = (binary_search_local - 1) - index;
+        }
+    }
+    u64 ret = 0;
+    if (element_ptr != searched_array + (ulong)array_size * 9) {
+        // LOG_INFO(Core_Hooking, "Search stopped at {}", (char*)*element_ptr);
+        for (int i = 0; i < 6; i++) {
+            LOG_INFO(Core_Hooking, "Heap struct part {}: {:#x}", i, element_ptr[i]);
+        }
+        if (strcmp(name, (char*)*element_ptr) == 0) {
+            ret = element_ptr[1];
+            u8 flags = *(searched_array_struct + 4);
+            if ((flags & 1) != 0) {
+                ret = ret + element_ptr[2];
+            }
+            if ((flags & 2) != 0) {
+                ret = ret + element_ptr[3];
+            }
+            if ((flags & 4) != 0) {
+                ret = ret + element_ptr[4];
+            }
+            if ((flags & 8) != 0) {
+                ret = ret + element_ptr[5];
+            }
+        }
+    }
+    // LOG_INFO(Core_Hooking, "Predicted return: {:#x}", ret);
 
     auto orig =
         (u64 PS4_SYSV_ABI (*)(u8*, char*))_SearchAndDoSomethingToMemoryByName_hook.Trampoline;
-    u64 ret = orig(searched_array_struct, name);
-    LOG_INFO(Core_Hooking, "ret: {:#x}", ret);
+    ret = orig(searched_array_struct, name);
+    LOG_INFO(Core_Hooking, "Return: {:#x}", ret);
     // if (strcmp(name, "editor") == 0 || strcmp(name, "extraEditorMemForGameHeap") == 0) {
     //     return ret == 0 ? 0x100000 : ret;
     // } else if (strcmp(name, "DebugHeap") == 0) {
@@ -76,7 +99,7 @@ void Initialize(Core::Module* mainModule) {
     _SearchAndDoSomethingToMemoryByName_hook =
         CreateHook(reinterpret_cast<void*>(EBOOT_MODULE_BASE - GHIDRA_OFFSET_ADJUSTMENT +
                                            DC_SearchAndDoSomethingToMemoryByName),
-                   (void*)&HOOK_SearchAndDoSomethingToMemoryByName);
+                   (void*)&HOOK_QueryHeapSize);
     EnableHook(&_SearchAndDoSomethingToMemoryByName_hook);
 
     initted = true;
