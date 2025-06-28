db3aacd60673a01476a462e97146a5312353d516
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-06-28 15:25:06 +0200

    refactor + HeapMetadata struct

--- src/core/game_hooks/dc_hooks.cpp
+++ src/core/game_hooks/dc_hooks.cpp
@@ -3,7 +3,7 @@
 
 #include "common/singleton.h"
 #include "core/game_hooks/LightHook.h"
-#include "core/game_hooks/gtscb_hooks.h"
+#include "core/game_hooks/dc_hooks.h"
 #include "core/linker.h"
 // #include "common/version.h"
 #include "core/file_format/psf.h"
@@ -34,27 +34,34 @@ u64 SHAD_NO_INLINE PS4_SYSV_ABI HOOK_QueryHeapSize(u8* searched_array_struct, ch
         strcmp(name, "DebugHeap") == 0) {
         int a = 0;
     }
-    u64* searched_array = *(u64**)(searched_array_struct + 0x820);
+    HeapMetadata* searched_array = *(HeapMetadata**)(searched_array_struct + 0x820);
     u32 array_size = *(u32*)(searched_array_struct + 0x818);
 
     // prints out the list of heap struct data
-    // for (int i = 0; i < array_size; i++) {
-    //     u64* heap_info = searched_array + (i * 9);
-    //     LOG_INFO(Core_Hooking, "------ Heap {} data ------", i);
-    //     LOG_INFO(Core_Hooking, "Name: {}", (char*)heap_info[0]);
-    //     LOG_INFO(Core_Hooking, "Base size: {:#x}", heap_info[1]);
-    //     for (int i = 2; i < 9; i++) {
-    //         LOG_INFO(Core_Hooking, "Part {}: {:#x}", i, heap_info[i]);
-    //     }
-    // }
+    for (int i = 0; i < array_size; i++) {
+        HeapMetadata heap_info = searched_array[i];
+        fmt::println("------ Heap {} data ------", i);
+        fmt::println("Name: {}", (char*)heap_info.name);
+        fmt::println("Base size: {:#x}", heap_info.base_size);
+        for (int i = 2; i < 7; i++) {
+            // fmt::println("Part {}: {:#x}", i, heap_info[i]);
+        }
+        constexpr const char* noname = "null";
+        if (heap_info.parent_heap_name) {
+            fmt::println("Parent heap: {}", (char*)heap_info.parent_heap_name);
+        } else {
+            fmt::println("Parent heap: {}", "null");
+        }
+        fmt::println("Part 8: {:#x}", heap_info.unk2);
+    }
 
     u64 index = (u64)array_size;
-    u64* element_ptr = searched_array;
+    HeapMetadata* element_ptr = searched_array;
     u64 binary_search_local;
     while (binary_search_local = index, 0 < (long)binary_search_local) {
         index = (long)binary_search_local / 2;
         // LOG_INFO(Core_Hooking, "Comparing \"{}\" to {}", (char*)element_ptr[index * 9], name);
-        s32 iVar2 = strcmp((char*)element_ptr[index * 9], name);
+        s32 iVar2 = strcmp(element_ptr[index].name, name);
         if (iVar2 < 0) {
             element_ptr = element_ptr + index * 9 + 9;
             index = (binary_search_local - 1) - index;
@@ -63,25 +70,38 @@ u64 SHAD_NO_INLINE PS4_SYSV_ABI HOOK_QueryHeapSize(u8* searched_array_struct, ch
     u64 ret = 0;
     if (element_ptr != searched_array + (ulong)array_size * 9) {
         // LOG_INFO(Core_Hooking, "Search stopped at {}", (char*)*element_ptr);
-        LOG_INFO(Core_Hooking, "Heap struct name: {}", (char*)element_ptr[0]);
-        LOG_INFO(Core_Hooking, "Heap struct base size: {:#x}", element_ptr[1]);
-        for (int i = 2; i < 9; i++) {
-            LOG_INFO(Core_Hooking, "Heap struct part {}: {:#x}", i, element_ptr[i]);
+        if (strcmp(name, "DebugHeap") == 0) {
+            LOG_INFO(Core_Hooking, "Spoofing size");
+            element_ptr->base_size = 0x30000000;
+        } else if (strcmp(name, "editor") == 0 || strcmp(name, "extraEditorMemForGameHeap") == 0) {
+            LOG_INFO(Core_Hooking, "Spoofing size");
+            element_ptr->base_size = 0x1000000;
         }
-        if (strcmp(name, (char*)*element_ptr) == 0) {
-            ret = element_ptr[1];
+        LOG_INFO(Core_Hooking, "Name: {}", element_ptr->name);
+        LOG_INFO(Core_Hooking, "Base size: {:#x}", element_ptr->base_size);
+        for (int i = 2; i < 7; i++) {
+            // LOG_INFO(Core_Hooking, "Part {}: {:#x}", i, element_ptr[i]);
+        }
+        if (element_ptr->parent_heap_name) {
+            LOG_INFO(Core_Hooking, "Parent heap: {}", element_ptr->parent_heap_name);
+        } else {
+            LOG_INFO(Core_Hooking, "Parent heap: {}", "null");
+        }
+        LOG_INFO(Core_Hooking, "Part 8: {:#x}", element_ptr->unk2);
+        if (strcmp(name, element_ptr->name) == 0) {
+            ret = element_ptr->base_size;
             u8 flags = *(searched_array_struct + 4);
             if ((flags & 1) != 0) {
-                ret = ret + element_ptr[2];
+                ret = ret + element_ptr->size_mod_1;
             }
             if ((flags & 2) != 0) {
-                ret = ret + element_ptr[3];
+                ret = ret + element_ptr->size_mod_2;
             }
             if ((flags & 4) != 0) {
-                ret = ret + element_ptr[4];
+                ret = ret + element_ptr->size_mod_3;
             }
             if ((flags & 8) != 0) {
-                ret = ret + element_ptr[5];
+                ret = ret + element_ptr->size_mod_4;
             }
         }
     }
--- src/core/game_hooks/dc_hooks.h
+++ src/core/game_hooks/dc_hooks.h
@@ -9,6 +9,18 @@
 
 namespace DCHooks {
 
+struct HeapMetadata {
+    char* name;
+    u64 base_size;
+    u64 size_mod_1;
+    u64 size_mod_2;
+    u64 size_mod_3;
+    u64 size_mod_4;
+    u64 unk1;
+    char* parent_heap_name;
+    u64 unk2;
+};
+
 void Initialize(Core::Module* module);
 
 } // namespace DCBHooks
