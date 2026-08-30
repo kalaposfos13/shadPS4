787d918bc7dac83c168b9e278f605c92fcdc4315
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-08-30 16:15:22 +0200

    +

--- src/core/libraries/composite/composite.cpp
+++ src/core/libraries/composite/composite.cpp
@@ -10,6 +10,7 @@
 #include "core/libraries/videoout/video_out.h"
 #include "core/memory.h"
 #include "video_core/amdgpu/liverpool.h"
+#include "common/alignment.h"
 
 extern std::unique_ptr<AmdGpu::Liverpool> liverpool;
 
@@ -19,6 +20,8 @@ void* sce_compositor_system_address = nullptr;
 size_t sce_compositor_system_size = 0;
 void* sce_compositor_video_address = nullptr;
 size_t sce_compositor_video_size = 0;
+
+static s64 frame_id = 0;
 
 s32 PS4_SYSV_ABI sceCompositorAllocateIndex() {
     LOG_ERROR(Lib_Composite, "(STUBBED) called");
@@ -52,7 +55,7 @@ s32 PS4_SYSV_ABI sceCompositorDeleteIndirectRenderTarget() {
 
 s32 PS4_SYSV_ABI sceCompositorFlush() {
     LOG_ERROR(Lib_Composite, "(STUBBED) called");
-    Libraries::VideoOut::sceVideoOutSubmitFlip(2, 0, 0, 0);
+    Libraries::VideoOut::sceVideoOutSubmitFlip(2, 0, 1, frame_id++);
     return ORBIS_OK;
 }
 
@@ -143,6 +146,8 @@ s32 PS4_SYSV_ABI sceCompositorHandleProcessEvents() {
     return ORBIS_OK;
 }
 
+static Kernel::OrbisKernelEqueue composite_flip_queue;
+
 s32 PS4_SYSV_ABI sceCompositorInit() {
     LOG_ERROR(Lib_Composite, "called");
     sce_compositor_system_address = nullptr;
@@ -156,9 +161,9 @@ s32 PS4_SYSV_ABI sceCompositorInit() {
         "sceComposite HLE buffer 2");
     sce_compositor_video_size = 1_GB;
 
-    constexpr u32 sce_composite_color_width = 1280;
-    constexpr u32 sce_composite_color_height = 720;
-    constexpr u32 color_target_size = sce_composite_color_width * sce_composite_color_height * 4;
+    constexpr u32 sce_composite_color_width = 1920;
+    constexpr u32 sce_composite_color_height = 1080;
+    constexpr u32 color_target_size = Common::AlignUp(sce_composite_color_width * sce_composite_color_height * 4, 16_KB);
 
     using namespace VideoOut;
     using namespace Kernel;
@@ -169,26 +174,23 @@ s32 PS4_SYSV_ABI sceCompositorInit() {
     // TODO: User proper flags when I emulate them
     sceKernelAllocateMainDirectMemory(color_target_size, 16_KB, 0, (s64*)&dmem_addr);
     sceKernelMapDirectMemory(&sce_composite_color_target_addr, color_target_size, 0, 0,
-                             (s64)dmem_addr, 0x1000);
+                             (s64)dmem_addr, 16_KB);
 
     ASSERT(sce_composite_color_target_addr != nullptr);
 
     BufferAttribute attrib = {};
-    attrib.pixel_format = PixelFormat::A8R8G8B8Srgb;
-    attrib.tiling_mode =
-        TilingMode::Linear; // the og comment says linear, but has 0 as the value, which is tiled?
-                            // either way, we're not getting to the point where it matters yet
-    attrib.aspect_ratio = 0;
-    attrib.width = sce_composite_color_width;
-    attrib.height = sce_composite_color_height;
-    attrib.pitch_in_pixel = sce_composite_color_width;
-    attrib.option = 0;
-    attrib.reserved0 = 0;
-    attrib.reserved1 = 0;
+    sceVideoOutSetBufferAttribute(&attrib, PixelFormat::A8R8G8B8Srgb, (u32)TilingMode::Linear, 0,
+                                  sce_composite_color_width, sce_composite_color_height,
+                                  sce_composite_color_width);
 
-    void* addrs[1] = {sce_composite_color_target_addr};
-    sceVideoOutOpen(0, 0, 0, nullptr);
-    sceVideoOutRegisterBuffers(2, 0, addrs, 1, &attrib);
+    void* addrs[2] = {sce_composite_color_target_addr, sce_composite_color_target_addr};
+    s32 video_out_handle =
+        sceVideoOutOpen(Libraries::UserService::ORBIS_USER_SERVICE_USER_ID_SYSTEM, 0, 0, nullptr);
+
+    int rc = sceKernelCreateEqueue(&composite_flip_queue, "composite flip queue");
+    sceVideoOutAddFlipEvent(composite_flip_queue, video_out_handle, 0);
+
+    sceVideoOutRegisterBuffers(video_out_handle, 0, addrs, 2, &attrib);
     return ORBIS_OK;
 }
 
@@ -214,7 +216,7 @@ s32 PS4_SYSV_ABI sceCompositorIsReady4K() {
 }
 
 s32 PS4_SYSV_ABI sceCompositorLockCommandBuffer() {
-    LOG_ERROR(Lib_Composite, "(STUBBED) called");
+    // LOG_ERROR(Lib_Composite, "(STUBBED) called");
     return ORBIS_OK;
 }
 
@@ -234,7 +236,7 @@ s32 PS4_SYSV_ABI sceCompositorQuit() {
 }
 
 s32 PS4_SYSV_ABI sceCompositorReleaseCommandBuffer() {
-    LOG_ERROR(Lib_Composite, "(STUBBED) called");
+    // LOG_ERROR(Lib_Composite, "(STUBBED) called");
     return ORBIS_OK;
 }
 
@@ -285,7 +287,7 @@ s32 PS4_SYSV_ABI sceCompositorSetEventCommand() {
 
 s32 PS4_SYSV_ABI sceCompositorSetFlipCommand() {
     LOG_ERROR(Lib_Composite, "called");
-    Libraries::VideoOut::sceVideoOutSubmitFlip(2, 0, 0, 0);
+    Libraries::VideoOut::sceVideoOutSubmitFlip(2, 0, 1, frame_id++);
     return ORBIS_OK;
 }
 
@@ -445,8 +447,7 @@ s32 sceApplicationSetCanvasHandle(s16 param_1, s32 param_2) {
 
 void RegisterLib(Core::Loader::SymbolsResolver* sym) {
 
-    LIB_FUNCTION("qTHiabfEukw", "libSceSysCore", 1, "libSceSysCore",
-                 sceApplicationSetCanvasHandle);
+    LIB_FUNCTION("qTHiabfEukw", "libSceSysCore", 1, "libSceSysCore", sceApplicationSetCanvasHandle);
 
     LIB_FUNCTION("G4Q8KNkb5XE", "libSceComposite", 1, "libSceComposite",
                  sceCompositorAllocateIndex);
